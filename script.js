4bd1b21a69f9830ad546927649f39a80d738205e
Saeed Noori <saeednoori13842005@gmail.com>
2025-11-01 23:13:32 +0330

    A new picture

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Saeed is one of ")
+function sum(a, b) {
+    return a * b
+}
+console.log(sum(4, 5))
+console.log(sum(5, 6))
+console.log(sum(20, 3))
+console.log(sum(2, 20))
+console.log(sum(25, 4))
+console.log(sum(9, 9))
