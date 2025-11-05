b0945f3079f5ee988560cfefabeaebf55b47b959
Saeed Noori <saeednoori13842005@gmail.com>
2025-11-05 22:24:01 +0330

    js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-"use strict"
-
+'use strict'
 var x = 100;
 function saysomething() {
-    console.log(x)
-    var x = 50
-    console.log(x)
+    var x = 90;
+    console.log(x);
 }
 saysomething()
-x = 90
+x = 80
 console.log(x)
