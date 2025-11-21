10f9899b17aaca910aac98806478de46c54be611
Saeed Noori <saeednoori13842005@gmail.com>
2025-11-22 00:19:50 +0330

    Objects in js :)

--- js/saeed.js
+++ js/saeed.js
@@ -1,3 +1,13 @@
 'use strict'
-
-console.log("saeed")
+var car = {
+    make: "America",
+    model: 1969,
+    "year-old": 1999,
+    details: {
+        colorS: ["red", "Yellow", "Black", "White"],
+        wheel: 4
+    }
+}
+car.name = "Ford"
+car["maximum-use"] = 30
+console.log(car["maximum-use"])
